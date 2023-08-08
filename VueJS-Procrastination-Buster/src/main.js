9d40e68318205c80a17c1d2d1ce72928944c193b
Casmir Onyeka <casmir293@gmail.com>
2023-08-09 00:38:53 +0100

    Configured Vue Vite, Bootstrap & SASS.

---
+++ VueJS-Procrastination-Buster/src/main.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap";
+import "@popperjs/core";
+import "./assets/main.scss";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const vm = createApp(App);
+vm.mount("#app");
