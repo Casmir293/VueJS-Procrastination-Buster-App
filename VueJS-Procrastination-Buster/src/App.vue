f9c6fbe49dc8315e3847e0f0897ed282e0439788
Casmir Onyeka <casmir293@gmail.com>
2023-08-09 21:44:58 +0100

    Added Light & Dark Theme Mode.

--- VueJS-Procrastination-Buster/src/App.vue
+++ VueJS-Procrastination-Buster/src/App.vue
@@ -1,13 +1,48 @@
 <template>
-  <header class="d-flex align-items-center shadow-lg">
-    <div>
-      <img src="/overcome_procrastination.png" alt="logo" class="m-2" />
-    </div>
-    <div class="">
-      <h1>Procrastination</h1>
-      <h3 class="text-danger">Buster <span class="text-dark">App</span></h3>
-    </div>
-  </header>
+  <div
+    class="content"
+    :class="theme === 'light' ? 'light-theme' : 'dark-theme'"
+  >
+    <header class="d-flex align-items-center shadow-lg">
+      <div>
+        <img src="/overcome_procrastination.png" alt="logo" class="m-2" />
+      </div>
+      <div class="">
+        <h1>Procrastination</h1>
+        <h3 class="text-danger">Buster <span class="text-dark">App</span></h3>
+      </div>
+    </header>
+
+    <main></main>
+
+    <footer></footer>
+  </div>
+  <div class="theme" @click="changeTheme">
+    <font-awesome-icon
+      icon="fa-solid fa-moon"
+      size="xl"
+      v-show="theme === 'dark'"
+    />
+    <font-awesome-icon
+      icon="fa-solid fa-sun"
+      size="xl"
+      v-show="theme === 'light'"
+    />
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      theme: "light",
+    };
+  },
+  methods: {
+    changeTheme() {
+      this.theme = this.theme === "dark" ? "light" : "dark";
+    },
+  },
+};
+</script>
--- VueJS-Procrastination-Buster/src/main.js
+++ VueJS-Procrastination-Buster/src/main.js
@@ -1,10 +1,22 @@
+//  Bootstrap
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap";
 import "@popperjs/core";
+
+//  SCSS
 import "./assets/scss/main.scss";
 
+//  Font Awesome
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faSun } from "@fortawesome/free-solid-svg-icons";
+library.add(faMoon, faSun);
+
+//  Vue App
 import { createApp } from "vue";
 import App from "./App.vue";
 
 const vm = createApp(App);
+vm.component("font-awesome-icon", FontAwesomeIcon);
 vm.mount("#app");
