754fb91ad5cf094a83633f339702685a3c9e62a9
Casmir Onyeka <casmir293@gmail.com>
2023-08-09 14:24:11 +0100

    Done with Header.

--- VueJS-Procrastination-Buster/src/App.vue
+++ VueJS-Procrastination-Buster/src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div></div>
+  <header class="d-flex align-items-center shadow-lg">
+    <div>
+      <img src="/overcome_procrastination.png" alt="logo" class="m-2" />
+    </div>
+    <div class="">
+      <h1>Procrastination</h1>
+      <h3 class="text-danger">Buster <span class="text-dark">App</span></h3>
+    </div>
+  </header>
 </template>
 
 <script></script>
-
-<style lang="scss" scoped></style>
