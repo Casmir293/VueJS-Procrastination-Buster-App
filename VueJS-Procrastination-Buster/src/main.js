23a21b973779f1c9a6a697603cb695ed55e5561f
Casmir Onyeka <casmir293@gmail.com>
2023-08-09 09:22:28 +0100

    SCSS folder and page icon added.

--- VueJS-Procrastination-Buster/src/main.js
+++ VueJS-Procrastination-Buster/src/main.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap";
 import "@popperjs/core";
-import "./assets/main.scss";
+import "./assets/scss/main.scss";
 
 import { createApp } from "vue";
 import App from "./App.vue";
