c428c7d527b30105183ce0acb2ec5de0c28468ff
Casmir Onyeka <casmir293@gmail.com>
2023-08-10 14:17:06 +0100

    Real Time and Date Added.

--- VueJS-Procrastination-Buster/src/components/Options.vue
+++ VueJS-Procrastination-Buster/src/components/Options.vue
@@ -5,13 +5,47 @@
       achieving your daily GOALS.
     </p>
     <div class="options mx-auto mb-5">
+      <div>
+        {{ formattedDate }}
+      </div>
       <div class="button">Shedule Events</div>
       <div class="button">Set Timer</div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
+  computed: {
+    formattedDate() {
+      const order = {
+        weekday: "short",
+        day: "2-digit",
+        month: "short",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true,
+      };
+
+      return this.date.toLocaleString("en-US", order);
+    },
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.date = new Date();
+    }, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/options";
