1de5498bb037d045ba7922d602618875743f533b
Casmir Onyeka <casmir293@gmail.com>
2023-08-10 15:23:18 +0100

    First Commit for Animated Words.

--- VueJS-Procrastination-Buster/src/components/Options.vue
+++ VueJS-Procrastination-Buster/src/components/Options.vue
@@ -2,7 +2,10 @@
   <div class="content">
     <p class="p-2 text-center my-5">
       Do you know that "a stich in time saves nine"? Let me assist you in
-      achieving your daily GOALS.
+      reaching your daily
+      <span class="typing-animation"
+        ><b>{{ currentPhrase }}</b></span
+      >
     </p>
     <div class="options mx-auto mb-5">
       <div>
@@ -18,10 +21,17 @@
 export default {
   data() {
     return {
+      //  Date & Time
       date: new Date(),
+
+      // Animated Text
+      phrases: ["GOALS!", "TARGETS!!", "SCHEDULE!!!"],
+      currentIndex: 0,
+      interval: null,
     };
   },
   computed: {
+    //  Date & Time
     formattedDate() {
       const order = {
         weekday: "short",
@@ -32,17 +42,43 @@ export default {
         second: "numeric",
         hour12: true,
       };
-
       return this.date.toLocaleString("en-US", order);
     },
+
+    // Animated Text
+    currentPhrase() {
+      return this.phrases[this.currentIndex];
+    },
   },
+
+  methods: {
+    //  Animated Text
+    startCycle() {
+      this.interval = setInterval(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.phrases.length;
+      }, 3000);
+    },
+    stopCycle() {
+      clearInterval(this.interval);
+    },
+  },
+
   mounted() {
+    //  Date & Time
     this.interval = setInterval(() => {
       this.date = new Date();
     }, 1000);
+
+    //  Animated Text
+    this.startCycle();
   },
+
   beforeDestroy() {
+    //  Date & Time
     clearInterval(this.interval);
+
+    //  Animated Text
+    this.stopCycle();
   },
 };
 </script>
